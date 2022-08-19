a6b615f5320b8821be81f708418145d1a96fce1a
Dipali <deepaligupta01703@gmail.com>
2022-08-19 13:31:14 +0530

    sign up

--- lib/Login&SignUp/sign_up.dart
+++ lib/Login&SignUp/sign_up.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/material.dart';
+class MyWidget extends StatefulWidget {
+  const MyWidget({Key? key}) : super(key: key);
 
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea (
+        child : Center(
+        child:Column(
+          children: [
+        //Welcome to All Sweets!
+        Text(
+          'Welcome To All Sweets',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+            ),
+        ),
+        SizedBox(height:10),
+           Text(
+          'We Hope taste all the deliciousness',
+          style: TextStyle(
+            fontSize: 20,
+            ),
+        ),
+        SizedBox(height:10),
+        //Email 
+TextField(
+decoration: InputDecoration(),
+),
+        //Password
+
+        //Sign-in
+
+        //Not a member? Register Now!
+      ],
+
+      ), 
+      ),
+    ),
+    );
+    
+  }
+}
