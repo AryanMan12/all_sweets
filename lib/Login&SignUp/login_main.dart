ec240b86b0d3dd4e9cfc3ed4e372f0a49852d98e
AryanMan12 <aryanaryan1212.ag@gmail.com>
2022-08-19 12:57:11 +0530

    added sign up page

---
+++ lib/Login&SignUp/login_main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
