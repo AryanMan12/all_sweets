3a14cc7da8d944a50a40fa7d04dae99ee1655dee
Dipali <deepaligupta01703@gmail.com>
2022-08-19 14:47:56 +0530

    sign up ui done

--- lib/Login&SignUp/sign_up.dart
+++ lib/Login&SignUp/sign_up.dart
@@ -1,12 +1,14 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-class MyWidget extends StatefulWidget {
-  const MyWidget({Key? key}) : super(key: key);
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<SignUpPage> createState() => _SignUpPageState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,13 +16,19 @@ class _MyWidgetState extends State<MyWidget> {
       body: SafeArea (
         child : Center(
         child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Icon(
+Icons.phone_android,
+size:100,
+            ),
+            SizedBox(height:70),
         //Welcome to All Sweets!
         Text(
           'Welcome To All Sweets',
           style: TextStyle(
             fontWeight: FontWeight.bold,
-            fontSize: 25,
+            fontSize: 30,
             ),
         ),
         SizedBox(height:10),
@@ -30,16 +38,97 @@ class _MyWidgetState extends State<MyWidget> {
             fontSize: 20,
             ),
         ),
-        SizedBox(height:10),
+        SizedBox(height:50),
+
         //Email 
-TextField(
-decoration: InputDecoration(),
+Padding (
+  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+  child: Container (
+  decoration: BoxDecoration(
+    color: Colors.grey[200],
+    border: Border.all(color: Colors.white),
+    borderRadius: BorderRadius.circular(12)
+    ),
+    child: Padding(
+      padding: const EdgeInsets.only(left:20.0),
+  child: TextField(
+decoration: InputDecoration(
+  border: InputBorder.none,
+  hintText: 'Enter your email',
 ),
+),
+),
+),
+),
+SizedBox(height: 10),
+
         //Password
+        Padding (
+  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+  child: Container (
+  decoration: BoxDecoration(
+    color: Colors.grey[200],
+    border: Border.all(color: Colors.white),
+    borderRadius: BorderRadius.circular(12)
+    ),
+    child: Padding(
+      padding: const EdgeInsets.only(left:20.0),
+  child: TextField(
+    obscureText: true,
+decoration: InputDecoration(
+  border: InputBorder.none,
+  hintText: 'Enter your password',
+),
+),
+),
+),
+),
+SizedBox(height: 10),
 
         //Sign-in
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child:Container(
+            padding:  EdgeInsets.all(20),
+          decoration: BoxDecoration(color: Colors.deepPurple[600],
+          borderRadius: BorderRadius.circular(20),
+          ),
+          child: Center(
+          child: Text(
+            'Sign in',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 18,
+              ),
+            ),
+
+
+          ),
+        ),
+        ),
+        SizedBox(height: 25),
 
         //Not a member? Register Now!
+Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+  Text(
+    'Not a member?',
+  style: TextStyle(
+      fontWeight: FontWeight.bold,
+  ),
+  ),
+  Text(
+    ' Register Now!',
+    style: TextStyle(
+      color: Colors.deepPurple[500],
+      fontWeight: FontWeight.bold,
+
+    ),
+  )
+],)
+
       ],
 
       ), 
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Login&SignUp/sign_up.dart';
 
 
 void main() {
@@ -11,58 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'All Sweetss',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      debugShowCheckedModeBanner:false ,
+      home: SignUpPage(),
     );
   }
 }
