c682df07c5a3c2ed7cb4d7109f4eceba8f719072
Sanchit <sanchitgurav100@gmail.com>
2022-08-23 23:15:23 +0530

    item cart changes

--- lib/Login&SignUp/sign_up.dart
+++ lib/Login&SignUp/sign_up.dart
@@ -94,7 +94,7 @@ class _SignUpPageState extends State<SignUpPage> {
                   onTap: () => Navigator.push(
                       context,
                       MaterialPageRoute(
-                          builder: (context) => MyWidget())),
+                          builder: (context) => ItemCard())),
                   child: Container(
                     padding: EdgeInsets.all(20),
                     decoration: BoxDecoration(
