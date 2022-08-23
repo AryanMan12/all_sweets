65e2ad5ad0e9611e209b13e8b56476759097f982
AryanMan12 <aryanaryan1212.ag@gmail.com>
2022-08-23 20:53:12 +0530

    splashscreen complete

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:all_sweets/Login&SignUp/sign_up.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -10,15 +13,22 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 5),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => SignUpPage())));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.pink[300],
-body: Center(
-  
-  child: Lottie.network(
-    'https://assets6.lottiefiles.com/packages/lf20_2ho5h68l.json'
-    ),
-),
+      body: Center(
+        child: Lottie.network(
+            'https://assets6.lottiefiles.com/packages/lf20_2ho5h68l.json'),
+      ),
     );
   }
 }
