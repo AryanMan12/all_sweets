c75844a77e1e7b9f005be68e657e53ef3c7bc1da
Dipali <deepaligupta01703@gmail.com>
2022-08-23 23:01:19 +0530

    images folder

--- lib/Customer/item_card.dart
+++ lib/Customer/item_card.dart
@@ -1,10 +1,10 @@
-import 'dart:html';
+
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
+class ItemCard extends StatelessWidget {
+  const ItemCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +24,7 @@ body:ListView(
         mainAxisSpacing: 15.0,
         childAspectRatio: 0.8,
         children: <Widget>[
-_buildCard("Gulab Jamun", "\ Rs.500", "assets/product[0].jpg", false, false, context)
+_buildCard("Gulab Jamun", "\ Rs.500", "assets/images/product[0].jpg", false, false, context)
         ],
         )
     ),
