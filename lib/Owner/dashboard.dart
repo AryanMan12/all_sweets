197673fe789f0ed4583ddb399922c61c95838dff
AryanMan12 <aryanaryan1212.ag@gmail.com>
2022-08-28 16:52:24 +0530

    Dashboard initial idea Complete

---
+++ lib/Owner/dashboard.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  List<FlSpot> totalPoints = [
+    const FlSpot(0, 4500),
+    const FlSpot(1, 4000),
+    const FlSpot(2, 5000),
+    const FlSpot(3, 3000),
+    const FlSpot(4, 5200),
+    const FlSpot(5, 4500),
+    const FlSpot(6, 4520),
+    const FlSpot(7, 7000),
+    const FlSpot(8, 5000),
+  ];
+
+  List<FlSpot> balancePoints = [
+    const FlSpot(0, 2500),
+    const FlSpot(1, 3000),
+    const FlSpot(2, 3500),
+    const FlSpot(3, 1200),
+    const FlSpot(4, 3000),
+    const FlSpot(5, 2200),
+    const FlSpot(6, 2500),
+    const FlSpot(7, 4500),
+    const FlSpot(8, 3500),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        title: Text(
+          "Dashboard",
+          style: TextStyle(
+              fontSize: 28, fontWeight: FontWeight.w500, color: Colors.black),
+        ),
+        elevation: 6,
+      ),
+      body: ListView(children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 10),
+          child: Center(
+            child: Text(
+              "Weekly Overview",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+          ),
+        ),
+        Container(
+          height: 300,
+          padding: EdgeInsets.all(10),
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            elevation: 8,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(8, 75, 8, 5),
+              child: LineChart(
+                LineChartData(
+                    lineTouchData: LineTouchData(enabled: true),
+                    borderData: FlBorderData(show: false),
+                    lineBarsData: [
+                      LineChartBarData(
+                        spots: totalPoints,
+                        isCurved: true,
+                        barWidth: 3,
+                        color: Colors.blue,
+                        belowBarData: BarAreaData(
+                          show: true,
+                          color: Colors.lightBlue.withOpacity(0.5),
+                        ),
+                        dotData: FlDotData(
+                          show: false,
+                        ),
+                      ),
+                      LineChartBarData(
+                        spots: balancePoints,
+                        isCurved: true,
+                        barWidth: 3,
+                        color: Colors.green,
+                        belowBarData: BarAreaData(
+                          show: true,
+                          color: Colors.lightGreen.withOpacity(0.5),
+                        ),
+                        dotData: FlDotData(
+                          show: false,
+                        ),
+                      )
+                    ],
+                    minY: 0,
+                    minX: 0,
+                    titlesData: FlTitlesData(
+                      bottomTitles: AxisTitles(
+                          axisNameWidget: Row(
+                            children: [
+                              Text("------",
+                                  style: TextStyle(color: Colors.blue)),
+                              Text("Total        "),
+                              Text("------",
+                                  style: TextStyle(color: Colors.green)),
+                              Text("Balance"),
+                            ],
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                          ),
+                          sideTitles: SideTitles(
+                            showTitles: true,
+                            reservedSize: 15,
+                            getTitlesWidget: (value, meta) {
+                              switch (value.toInt()) {
+                                case 1:
+                                  return Text('Sun');
+                                case 2:
+                                  return Text('Mon');
+                                case 3:
+                                  return Text('Tue');
+                                case 4:
+                                  return Text('Wed');
+                                case 5:
+                                  return Text('Thu');
+                                case 6:
+                                  return Text('Fri');
+                                case 7:
+                                  return Text('Sat');
+                                default:
+                                  return Container();
+                              }
+                            },
+                          )),
+                      leftTitles: AxisTitles(
+                        sideTitles: SideTitles(
+                          showTitles: true,
+                          reservedSize: 40,
+                          getTitlesWidget: (value, meta) {
+                            return Text('${value.toInt()}');
+                          },
+                        ),
+                      ),
+                      topTitles:
+                          AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                      rightTitles:
+                          AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                    ),
+                    gridData: FlGridData(show: false)),
+                swapAnimationDuration: Duration(milliseconds: 150),
+                swapAnimationCurve: Curves.linear,
+              ),
+            ),
+          ),
+        ),
+        Divider(),
+        Padding(
+          padding: const EdgeInsets.only(top: 2, bottom: 6),
+          child: Center(
+            child: Text(
+              "Monthly Overview",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            Text(
+              "Total Earned",
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w800),
+            ),
+            Text(
+              "Total Balance",
+              style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w800),
+            ),
+            Text(
+              "Total Expense",
+              style: TextStyle(
+                  color: Colors.red, fontSize: 18, fontWeight: FontWeight.w800),
+            )
+          ],
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        ),
+        Row(
+          children: [
+            Text(
+              "12000",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+            Text(
+              "5000",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            ),
+            Text(
+              "7000",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+            )
+          ],
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+        )
+      ]),
+    );
+  }
+}
---
+++ lib/Owner/owner_main.dart
@@ -0,0 +1,59 @@
+import 'package:all_sweets/Owner/dashboard.dart';
+import 'package:flutter/material.dart';
+
+class OwnerBottomNav extends StatefulWidget {
+  const OwnerBottomNav({Key? key}) : super(key: key);
+
+  @override
+  State<OwnerBottomNav> createState() => _OwnerBottomNavState();
+}
+
+class _OwnerBottomNavState extends State<OwnerBottomNav> {
+  int pageNum = 0;
+
+  static const List<Widget> _Pages = <Widget>[Dashboard()];
+
+  void _onPageSelected(int index) {
+    setState(() {
+      pageNum = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: _Pages.elementAt(pageNum),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        iconSize: 30,
+        elevation: 0,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.black,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.bar_chart, size: 28),
+              label: '',
+              backgroundColor: Colors.white),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.event_note, size: 28),
+              label: '',
+              backgroundColor: Colors.white),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.edit_note, size: 28),
+              label: '',
+              backgroundColor: Colors.white),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.table_chart, size: 28),
+              label: '',
+              backgroundColor: Colors.white),
+        ],
+        currentIndex: pageNum,
+        onTap: _onPageSelected,
+      ),
+    );
+  }
+}
